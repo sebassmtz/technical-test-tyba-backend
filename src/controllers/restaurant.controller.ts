7c48cd99fd48a2245bd49e49d1c9020020054d2a
sebassmtz <sebastian0197333@gmail.com>
2024-09-01 10:39:24 -0500

    Se creo la API de obtener restaurantes, segun las coordenadas o ciudad

---
+++ src/controllers/restaurant.controller.ts
@@ -0,0 +1,47 @@
+import dotenv from "dotenv"
+import { Request, Response } from "express"
+import axios from "axios"
+
+dotenv.config()
+
+export class Restaurantcontroller {
+  public async getRestaurantForUbication(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
+    // Obtener ciudad o coordenadas de la petición
+    const city = req.query.city as string
+    const lat = req.query.lat as string
+    const lng = req.query.lng as string
+
+    if (!city && (!lat || !lng)) {
+      return res
+        .status(400)
+        .json({ error: "Debes proporcionar una ciudad o coordenadas." })
+    }
+
+    const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+    try {
+      let location = `${lat},${lng}`
+      // Si se proporciona una ciudad, obtener sus coordenadas
+      if (city) {
+        const geocodeUrl = `${process.env.GEOCODE_URL}/json?address=${encodeURIComponent(
+          city
+        )}&key=${GOOGLE_API_KEY}`
+        const geocodeResponse = await axios.get(geocodeUrl)
+        const { lat, lng } = geocodeResponse.data.results[0].geometry.location
+        location = `${lat},${lng}`
+      }
+      // Buscar restaurantes cerca de la ubicación
+      const placesUrl = `${process.env.PLACES_URL}/json?location=${location}&radius=1500&type=restaurant&key=${GOOGLE_API_KEY}`
+      const placesResponse = await axios.get(placesUrl)
+      const restaurants = placesResponse.data.results
+
+      return res.json(restaurants)
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "Ocurrió un error al obtener los restaurantes." })
+    }
+  }
+}
--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -2,6 +2,7 @@ import { Router } from "express"
 import healthRouter from "./health.router"
 import authRouter from "./auth.router"
 import userRouter from "./user.router"
+import restaurantRouter from "./restaurant.router"
 
 const router = Router()
 
@@ -33,6 +34,7 @@ export default function (app: Router): Router {
   healthRouter(app)
   authRouter(app)
   userRouter(app)
+  restaurantRouter(app)
 
   app.get("/", (req, res) => {
     res.status(200).json({ message: "Welcome to the API" })
